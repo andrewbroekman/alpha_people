2130cc87d2e7dc7ee7dca0cdd97f8c8420a0b78f
RentonMcIntyre <rentonmcintyre@yahoo.com>
2016-03-16 22:28:40 +0200

    Added ResearcherCategory class
    Defines a Researcher Category, which relies on a current
    ResearchCategoryState

---
+++ ResearcherCategory.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codinginfinity.people;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Renton Mcintyre (u14312710)
+ */
+
+/**
+ * Defines the ResearcherCategory object
+ */
+@Stateless
+@LocalBean
+public class ResearcherCategory {
+    /**
+    *  Defines the no-args constructor for a RsearcherCategory
+    *  Protected, as it should not be used
+    *  (especially not outside of the people package)
+    */
+    protected ResearcherCategory() {}
+    
+    /**
+    * Defines the the interface for standard constructor 
+    * for a ResearcherCategory that takes
+    * a single ResearchCategoryState as a starting point
+    * @param    start   A state to be added to the state list from the initial point
+    */
+    public ResearcherCategory(ResearchCategoryState start) 
+    {
+        states = new ArrayList<>();
+        this.states.add(start);
+    }
+    
+    
+    /**
+     * Returns the list of states for this ResearcherCategory
+     * @return The list of states for this ResearcherCategory
+     */
+    public ArrayList<ResearchCategoryState> getStates() { return this.states; }
+    
+    /**
+     * memberVariables
+     */
+    
+    /**
+     * The list of saved states of this ResearcherCategory
+     */
+    private ArrayList<ResearchCategoryState> states;
+    
+}
