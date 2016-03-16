3b74cfa837e2289281886280ea3d050f1990e1e8
RentonMcIntyre <rentonmcintyre@yahoo.com>
2016-03-16 22:17:40 +0200

    Created Entity interface
    This interface defines any being and is implemented by things such as
    Group and Person

---
+++ Entity.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codinginfinity.people;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Renton Mcintyre (u14312710)
+ */
+
+/**
+ * Defines an entity (any being with substance, such as a Person or Group)
+ */
+@Stateless
+@LocalBean
+public interface Entity {
+
+}
