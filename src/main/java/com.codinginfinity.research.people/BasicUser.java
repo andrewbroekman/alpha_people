0c43a5e02b940054b5abfa3b677856ae8ebbe906
RentonMcIntyre <rentonmcintyre@yahoo.com>
2016-03-19 23:04:08 +0200

    Just changed package name correctly for now
    Interfaces will be sorted completely in the morning

--- src/main/java/com.codinginfinity.research.people/BasicUser.java
+++ src/main/java/com.codinginfinity.research.people/BasicUser.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.codinginfinity.people;
+package com.codinginfinity.research.people;
 
 /**
  * A Basic user type with normal levels of access
--- src/main/java/com.codinginfinity.research.people/User.java
+++ src/main/java/com.codinginfinity.research.people/User.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.codinginfinity.people;
+package com.codinginfinity.research.people;
 
 /**
  * An object that has the ability to add and remove things in database
